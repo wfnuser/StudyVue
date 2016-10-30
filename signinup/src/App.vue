d4808522afd964ec43b38e7b304409d366de275d
wfnuser <wfnuser@126.com>
2016-10-30 23:52:57 +0800

    [FEATURE] something about router

--- signinup/src/App.vue
+++ signinup/src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
     <h1 v-text="title"></h1>
+    <p>
+      <!-- 使用 router-link 组件来导航. -->
+      <!-- 通过传入 `to` 属性指定链接. -->
+      <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
+      <router-link to="/foo">Go to Foo</router-link>
+      <router-link to="/bar">Go to Bar</router-link>
+    </p>
     <li v-for="item in items" v-bind:class="{finished: item.isFinished}" v-on:click="toggleFinish(item)">{{item.label}}</li>
+    <router-view></router-view>
   </div>
 </template>
 
